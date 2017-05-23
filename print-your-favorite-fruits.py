d83540f9517b3fd9b2cdd45bd994db617de4878a
LukeHur <lukehur@seoulchristian.org>
2017-05-23 10:58:07 +0900

    Update print-your-favorite-fruits.py

--- print-your-favorite-fruits.py
+++ print-your-favorite-fruits.py
@@ -1,1 +1,3 @@
-print "strawberry\nkiwi
+print "strawberry\nkiwi" # it writes strawberry and kiwi in a new line
+print "peach\ncherry" # it writes peach and cherry in a new line
+print "coconut" # it writes coconut in new line
